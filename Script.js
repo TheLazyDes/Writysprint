cdb86b8a1eeab3ad19f972e0e5809f23d349fae9
Aluko Olamide <olaluko20@gmail.com>
2026-02-13 13:26:49 -0800

    creation of project html elements as well as timer functionality for different buttons

---
+++ Script.js
@@ -0,0 +1,66 @@
+let button_5_min = document.getElementById("5-minutes");
+let button_10_min = document.getElementById("10-minutes");
+let button_25_min = document.getElementById("25-minutes");
+let button_custom = document.getElementById("Custom-button");
+
+let first_page=document.getElementById('select-sprint');
+let sprint_display=document.getElementById('sprint-display');
+
+
+let timer; // store interval globally
+
+function displayTimer(time){
+    const minutes = Math.floor(time / 60);
+    const seconds = time % 60;
+
+    sprint_display.textContent =
+        `${minutes} : ${seconds.toString().padStart(2, "0")}`;
+}
+
+function countdown(time){
+
+    clearInterval(timer); // stop previous timer if running
+
+    displayTimer(time); // show initial time
+
+    timer = setInterval(() => {
+
+        if (time > 0){
+            time--;
+            displayTimer(time);
+        } else {
+            clearInterval(timer);
+            sprint_display.textContent = "Time up!";
+        }
+
+    }, 1000);
+}
+
+
+
+
+
+button_5_min.addEventListener('click', ()=>{
+time=300;
+countdown(time);
+console.log("Your 5 minuted have started")
+})
+
+
+button_10_min.addEventListener('click', ()=>{
+time=600;
+countdown(time);
+console.log("Your 10 minuted have started")
+})
+
+button_25_min.addEventListener('click', ()=>{
+time=1500;
+countdown(time);
+console.log("Your 25 minuted have started")
+})
+
+button_custom.addEventListener('click', ()=>{
+    countdown(time);
+console.log("You have clicked")
+})
+    
